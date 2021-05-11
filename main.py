1b085fbd9375977b598f6fd58d254237843ed5eb
Joey Bishop <>
2021-05-11 01:11:33 +0000

    Adds interactive zerg rush

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import discord
 import os
 import random
+import time
 
 client = discord.Client()
 gameList = []
+zergProne = False
+timeSinceLastDebug = time.time()
 
 @client.event
 async def on_ready():
@@ -11,13 +14,38 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  global zergProne
+  global timeSinceLastDebug
+
   if (message.author == client.user):
     return
 
-  response = 'ERROR! That is not a valid command! Please retype your command, or consult my available commands with \'!help\'.'
+  # About every 22 hrs and 30 mins we're gonna get zerg prone
+  if (not zergProne and time.time() > timeSinceLastDebug + 81000):
+    zergProne = True
+  # Every even hour after, a zergling will infest the list of saved games
+  elif (zergProne and (timeSinceLastDebug % 7200) == 0):
+    saveGameName('zergling')
 
-  if message.content.startswith('!help'):
+  sender = str(message.author)
+
+  response = 'ERROR! That is not a valid command! Please retype your command, or consult my available commands with \'!help GAMEINATOR\'.'
+
+  if message.content.startswith('!help GAMEINATOR'):
     response = getHelp()
+
+  if message.content.startswith('/debug'):
+    await message.channel.send('Running diagnostics...')
+    time.sleep(2)
+    if (not zergProne):
+      response = 'No zerglings found here! Thank you for being vigilant ' + sender + '.'
+    else:
+      zergProne = False
+      await message.channel.send('ERR█R! Zerglings everywh█re!')
+      await message.channel.send('La█nching an█ivirus s█ftware...')
+      time.sleep(9)
+      response = 'Zerglings eradicated. I would have been assimilated if it weren\'t for you ' + sender + '!'
+      timeSinceLastDebug = time.time()
 
   if message.content.startswith('/feed '):
     saveGameName(message.content[6:len(message.content)])
@@ -38,7 +66,7 @@ async def on_message(message):
   await message.channel.send(response)
 
 def getHelp():
-  return ('I am {0.user}. I suggest games at random when prodded. You can also feed me games that you would like to play.\n\n------------------\n\n/feed <game>\nAdds the game to my random storage.\n\n/prod <quantity?>\nGets a random game from my storage. You can prod an optional quantity from one to three.\n\n------------------\n\n'.format(client))
+  return ('I am {0.user}. I suggest games at random when prodded. You can feed me games that you would like to play. Prone to zerg rushes if not properly monitored.\n\n------------------\n\n/debug\nRemoves any zerglings from my shell.\n\n/feed <game>\nAdds the game to my random storage.\n\n/prod (quantity)\nGets a random game from my storage. You can prod an optional quantity from one to three.\n\n/storage\nLists all games in my random storage.\n\n/vomit <game>\nRemoves a game from my random storage.\n\n------------------\n\n'.format(client))
 
 def getRandomGames(number):
   prodCount = int(number)
